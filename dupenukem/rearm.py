c1d7760e785c52b85668008c71079cd4ef3155df
Aaron Zaborac <aaron.zaborac@outlook.com>
2020-01-11 21:13:44 -0500

    Renamed rearm value in rearm.py to actually use the correct image.

--- dupenukem/rearm.py
+++ dupenukem/rearm.py
@@ -1,4 +1,4 @@
 # For testing and whatnot. Not actually part of the main script.
 import os, shutil
 
-shutil.copy('./backups/img1_dupe.png', os.getcwd())
+shutil.copy('./backups/img1 - Copy.jpg', os.getcwd())
